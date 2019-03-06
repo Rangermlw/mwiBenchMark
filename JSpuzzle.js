c6a5ac39f21ab6f0808210b7c1a0e98a2b8f117a
Micah Wald <micahwald@gmail.com>
2019-03-06 15:37:43 -0600

    initial commit

---
+++ JSpuzzle.js
@@ -0,0 +1,37 @@
+var listOne = {
+   mattJohnson : 'Matt Johnson',
+   bartPaden : 'Bart Paden',
+   ryanDoss : 'Ryan Doss',
+   michaelSpangler : 'Micheal Spangler'
+ };
+
+ var listTwo= {
+    mattJohnson : 'Matt Johnson',
+    bartPaden : 'Bart Paden',
+    jordanHeigle : 'Jordan Heigle',
+    tylerViles : 'Tyler Viles'
+  };
+
+function puzzle (x, y) {
+  return Object.assign({}, x, y);
+}
+
+var merged = puzzle(listOne, listTwo);
+
+merged = Object.values(merged);
+
+merged.forEach(function(item, key) {
+  var merged = item;
+});
+
+var listDisplay = " ";
+for(i = 0; i < merged.length; i ++) {
+  listDisplay += '<li>' + merged[i] + '</li>'
+}
+
+
+
+console.log(merged);
+
+var message = document.getElementById('names')
+message.innerHTML = listDisplay;
